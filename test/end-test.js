fc1515915abe49a91ff1deb51f902dbe37fdbbd5
Bilal Çınarlı <bcinarli@gmail.com>
2018-01-12 22:56:29 +0100

    End method  unit tests

--- test/end-test.js
+++ test/end-test.js
@@ -1,3 +1,16 @@
 /**
  * end-test
  **/
+
+describe('End', () => {
+    let endElem = uxr('#end');
+
+    it('should return the selection itself if selection is not filtered', () => {
+        expect(endElem.end()).to.be.equal(endElem);
+    });
+
+    it('should revert the previously selected element', () => {
+        let endInner = endElem.find('#end-inner');
+        expect(endInner.end()).to.be.equal(endElem);
+    });
+});
