5f25aa4e0a821328d48f5550d036ad695953fa68
Bilal Çınarlı <bcinarli@gmail.com>
2018-01-27 09:47:13 +0100

    Fallback to getAttribute removed since dataset is supported for all uxr target browsers

--- src/data.js
+++ src/data.js
@@ -8,17 +8,12 @@ _.extend.data = function (name, value) {
     let item = this.el[0];
     let domName = toDomString(name);
 
-    if (typeof item.dataset !== 'undefined') {
-        if (typeof value !== 'undefined') {
-            item.dataset[domName] = value;
-        }
-
-        else {
-            return item.dataset[domName];
-        }
+    if (typeof value !== 'undefined') {
+        item.dataset[domName] = value;
+        return this;
     }
 
     else {
-        return item.getAttribute('data-' + name);
+        return item.dataset[domName];
     }
 };
