0c258ef3de21dc97c3013d4e69af6a6d7faf9dea
Bilal Çınarlı <bcinarli@gmail.com>
2018-02-09 11:53:40 +0100

    Code refactoring, removing code duplication in dimensons.js, fixes #9, #10, #11

--- src/dimensions.js
+++ src/dimensions.js
@@ -4,66 +4,56 @@
 
 /* global removeUnit */
 
-const contentSize = ({stack, type, newSize}) => {
-    let client = type === 'width' ? 'clientWidth' : 'clientHeight';
-    let styleFirst = type === 'width' ? 'paddingLeft' : 'paddingTop';
-    let styleLast = type === 'width' ? 'paddingRight' : 'paddingBottom';
+const _contentSize = type => {
+    return function (newSize) {
+        let client = type === 'width' ? 'clientWidth' : 'clientHeight';
+        let styleFirst = type === 'width' ? 'paddingLeft' : 'paddingTop';
+        let styleLast = type === 'width' ? 'paddingRight' : 'paddingBottom';
 
-    if (stack.length === 0) {
-        return false;
-    }
+        if (this.length === 0) {
+            return false;
+        }
 
-    if (newSize) {
-        stack.el.forEach(item => item.style[type] = newSize);
+        if (newSize) {
+            this.el.forEach(item => item.style[type] = newSize);
 
-        return stack;
-    }
+            return this;
+        }
 
-    return stack.el[0][client] - removeUnit(stack.el[0].style[styleFirst]) - removeUnit(stack.el[0].style[styleLast]);
+        return this.el[0][client] - removeUnit(this.el[0].style[styleFirst]) - removeUnit(this.el[0].style[styleLast]);
+    };
 };
 
-const clientSize = ({stack, type}) => {
-    return stack.length > 0 ? stack.el[0][type] : false;
+const _clientSize = type => {
+    return function () {
+        return this.length > 0 ? this.el[0][type] : false;
+    };
 };
 
-const offsetSize = ({stack, type, margins}) => {
-    let styleFirst = margins ? 'marginLeft' : 'marginTop';
-    let styleLast = margins ? 'marginRight' : 'marginBottom';
+const _offsetSize = type => {
+    return function (includeMargins = false) {
+        let styleFirst = includeMargins ? 'marginLeft' : 'marginTop';
+        let styleLast = includeMargins ? 'marginRight' : 'marginBottom';
 
-    if (stack.length === 0) {
-        return false;
-    }
+        if (this.length === 0) {
+            return false;
+        }
 
-    let el = stack.el[0];
-    let sizeType = el[type];
+        let el = this.el[0];
+        let sizeType = el[type];
 
-    if (margins) {
-        sizeType += removeUnit(el.style[styleFirst]) + removeUnit(el.style[styleLast]);
-    }
+        if (includeMargins) {
+            sizeType += removeUnit(el.style[styleFirst]) + removeUnit(el.style[styleLast]);
+        }
 
-    return sizeType;
+        return sizeType;
+    };
 };
 
-_.extend.contentWidth = _.extend.width = function (newWidth) {
-    return contentSize({stack: this, type: 'width', newSize: newWidth});
-};
+_.extend.contentWidth = _.extend.width = _contentSize('width');
+_.extend.clientWidth = _.extend.innerWidth = _clientSize('clientWidth');
+_.extend.offsetWidth = _.extend.outerWidth = _offsetSize('offsetWidth');
 
-_.extend.clientWidth = _.extend.innerWidth = function () {
-    return clientSize({stack: this, type: 'clientWidth'});
-};
-
-_.extend.offsetWidth = _.extend.outerWidth = function (includeMargins = false) {
-    return offsetSize({stack: this, type: 'offsetWidth', margins: includeMargins});
-};
-
-_.extend.contentHeight = _.extend.height = function (newHeight) {
-    return contentSize({stack: this, type: 'height', newSize: newHeight});
-};
-
-_.extend.clientHeight = _.extend.innerHeight = function () {
-    return clientSize({stack: this, type: 'clientHeight'});
-};
-
-_.extend.offsetHeight = _.extend.outerHeight = function (includeMargins = false) {
-    return offsetSize({stack: this, type: 'offsetHeight', margins: includeMargins});
-};
+_.extend.contentHeight = _.extend.height = _contentSize('height');
+_.extend.clientHeight = _.extend.innerHeight = _clientSize('clientHeight');
+_.extend.offsetHeight = _.extend.outerHeight = _offsetSize('offsetHeight');
